a5e07b8afae23cb795b08a22a75fce8e4d554033
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-03-31 09:58:41 +0200

    created flags component

---
+++ src/Components/settings/flags.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import { useSelector } from "react-redux";
+
+
+export const Flags = () => {
+    return (
+        <div className="flags-container">Flags</div>
+    )
+};
--- src/Components/settings/settings.js
+++ src/Components/settings/settings.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Strength} from "./strength";
 import {Slider} from "./slider";
+import {Flags} from "./flags";
 // import { useSelector } from "react-redux";
 
 
@@ -9,6 +10,7 @@ export const Settings = () => {
         <div className="settings-container">
             Settings
             <Slider />
+            <Flags />
             <Strength />
            </div>
     )
