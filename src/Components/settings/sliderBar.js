541301efca71298a21f0ba893d2dc1496e4021a5
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-04-03 13:36:58 +0200

    created new slide bar

--- src/Components/settings/sliderBar.js
+++ src/Components/settings/sliderBar.js
@@ -5,9 +5,15 @@ import logo from "../../assets/images/icon-copy.svg";
 
 export const SliderBar = () => {
   return (
-    <div>
-      <div className="slider-bar"></div>
-      <div className="circle"></div>
+    <div className="slider-bar">
+      <input
+        type="range"
+        min="1"
+        max="100"
+        value="50"
+        className="slider"
+        id="myRange"
+      />
     </div>
   );
 };
