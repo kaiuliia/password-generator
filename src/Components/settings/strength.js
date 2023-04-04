4930d3bc25a02fb1256dbd552d3f9ba18146c9a7
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-04-04 14:14:11 +0200

    added strength component

--- src/Components/settings/strength.js
+++ src/Components/settings/strength.js
@@ -1,7 +1,18 @@
 import React from "react";
 // import { useSelector } from "react-redux";
 import "../../App.css";
+import logo from "../../assets/images/icon-copy.svg";
 
 export const Strength = () => {
-  return <div className="strength-container">Strength</div>;
+  return (
+    <div className="strength-container">
+      <div className="left">
+        <p>password is here</p>
+      </div>
+      <div className="right">
+        <p className="copiedWord">MEDIUM</p>
+        <div className="colorBlock"></div>
+      </div>
+    </div>
+  );
 };
