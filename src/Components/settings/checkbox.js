007398d8e13d73e54186fefaabc3b3fa07018a4a
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-04-15 07:50:31 +0200

    upd up case

--- src/Components/settings/checkbox.js
+++ src/Components/settings/checkbox.js
@@ -3,22 +3,32 @@ import React from "react";
 import "../../App.css";
 import { useDispatch, useSelector } from "react-redux";
 
-import { lowCaseChecked, numCaseChecked } from "./settingSlice";
+import { lowCaseChecked, numCaseChecked, upCaseChecked } from "./settingSlice";
 export const Checkbox = () => {
   const dispatch = useDispatch();
   const lowCheck = useSelector((state) => state.passw.incLow);
   const numCheck = useSelector((state) => state.passw.incNum);
-
+  const upCheck = useSelector((state) => state.passw.incUpper);
   const checkHandlier = () => {
     dispatch(lowCaseChecked());
   };
   const checkNumHandlier = () => {
     dispatch(numCaseChecked());
   };
+
+  const checkUppHandlier = () => {
+    dispatch(upCaseChecked());
+  };
+
   return (
     <div className="checkboxContainer">
       <label class="container">
-        <input type="checkbox" className="firstCheck" />
+        <input
+          type="checkbox"
+          onChange={checkUppHandlier}
+          checked={upCheck}
+          className="firstCheck"
+        />
         <span>Include Uppercase Letters</span>
       </label>
 
