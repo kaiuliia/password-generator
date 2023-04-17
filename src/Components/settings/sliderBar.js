01945a2ca719186d71128138f83d90ddf104e42b
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-04-17 10:13:02 +0200

    upd sliderChange with redux (without a function yet)

--- src/Components/settings/sliderBar.js
+++ src/Components/settings/sliderBar.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { useState } from "react";
-import { useSelector } from "react-redux";
+
+import { useDispatch, useSelector } from "react-redux";
+import { lowCaseChecked, sliderChange } from "./settingSlice";
 
 export const SliderBar = () => {
-  const [value, changeValue] = useState(10);
-  const passwordLength = useSelector((state) => state.passw.length);
-
-  // const input = document.querySelector("#myRange");
-  // value.textContent = input.value;
-  // input.addEventListener("input", (event) => {
-  //   value.textContent = event.target.value;
-  // });
+  // const [value, changeValue] = useState(10);
+  const value = useSelector((state) => state.passw.length);
+  const dispatch = useDispatch();
+  const changeSlider = () => {
+    dispatch(sliderChange());
+  };
 
   return (
     <div className="slider-bar">
@@ -21,7 +20,7 @@ export const SliderBar = () => {
         value={value}
         className="slider"
         id="myRange"
-        onChange={(e) => changeValue(e.target.value)}
+        onChange={changeSlider}
       />
     </div>
   );
